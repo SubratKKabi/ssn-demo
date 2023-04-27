3cf6a83e7a647f312d8e1da1ee03636a370b90a0
Rodrigo Tenório <rodrigotenorio@gmail.com>
2023-04-27 17:14:00 -0300

    rt changes

--- spring-boot-server/src/main/java/com/demo/spring/jpa/h2/SpringBootSsnApplication.java
+++ spring-boot-server/src/main/java/com/demo/spring/jpa/h2/SpringBootSsnApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplicationn
 @ComponentScan(basePackages = {"com.demo.spring"})
 public class SpringBootSsnApplication {
 
